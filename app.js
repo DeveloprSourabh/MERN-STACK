d9a3d90df36fc12349ba5860e42a6550318543c2
Sourabh <developr.sourabh@gmail.com>
2022-09-05 15:05:38 +0530

    run the server on local host

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("HELLO WORLD FROM SOURABH.");
+});
+
+app.listen(3000, () => {
+  console.log("SERVER IS RUNNING ON PORT 3000");
+});
