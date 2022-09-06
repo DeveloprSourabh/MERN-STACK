b72a28d1ae4b0480b0372723a39ddf75f3990f3e
Sourabh <developr.sourabh@gmail.com>
2022-09-06 15:40:57 +0530

    add component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+};
+
+export default App;
